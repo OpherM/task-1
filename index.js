78679f2e1a419c4bcfe6ebc5d960e3bbb4aa508a
OpherM <47371176+OpherM@users.noreply.github.com>
2023-09-08 16:34:30 +0100

    second

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const currentDate = new Date();
 const currentDay = daysOfWeek[currentDate.getUTCDay()];
 const currentUTCTime = currentDate.getTime();
 
+function updateUTCTime() {
+    const currentDate = new Date();
+    const currentUTCTime = currentDate.getTime();
+
+    // Update the content of the element with data-testid attribute
+    document.querySelector('[data-testid="currentUTCTime"]').textContent = `Current UTC Time: ${currentUTCTime} ms`;
+}
 
 document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = `Current Day: ${currentDay}`;
 document.querySelector('[data-testid="currentUTCTime"]').textContent = `Current UTC Time: ${currentUTCTime} ms`;
+
+updateUTCTime();
+
+setInterval(updateUTCTime, 500);
+
